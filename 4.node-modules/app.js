ba9b498af5375432d38aba11f048be49fdda23c0
YoungKi Lyu <youngkiu@gmail.com>
2022-02-23 21:10:19 +0900

    type of `events` export

---
+++ 4.node-modules/app.js
@@ -0,0 +1,6 @@
+const events = require("events");
+console.log(`typeof events: ${typeof events}`);
+const path = require("path");
+console.log(`typeof path: ${typeof path}`);
+const fs = require("fs");
+console.log(`typeof fs: ${typeof fs}`);
